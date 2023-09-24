45e06f7508ba37778820822cad7c47cc68eb5c62
dimasm93 <software.dimas_m@icloud.com>
2023-09-24 20:33:49 +0700

    update script template

---
+++ webapp-upload/src/main/java/com/maryanto/dimas/training/nke/upload/WebappUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.training.nke.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappUploaderApplication.class, args);
+    }
+
+}
---
+++ webapp-upload/src/main/java/com/maryanto/dimas/training/nke/upload/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.maryanto.dimas.training.nke.upload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model mav) {
+        mav.addAttribute("user", "Dimas Maryanto");
+        mav.addAttribute("tanggal", LocalDate.now().toString());
+        return "index";
+    }
+}
